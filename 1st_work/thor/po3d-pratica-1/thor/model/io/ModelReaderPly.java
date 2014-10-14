32861ddec52b699bb5cfa7b72b7eaa0f71ba6959
RMD <ruimd2.91@gmail.com>
2014-10-14 23:55:19 +0100

    Simple implementation of PLY model reader (only reads and uses the essential parts). Added converted .obj to .ply objects

--- 1st_work/thor/po3d-pratica-1/thor/model/io/ModelReaderPly.java
+++ 1st_work/thor/po3d-pratica-1/thor/model/io/ModelReaderPly.java
@@ -1,9 +1,15 @@
 // Copyright 2012 Pedro B. Pascoal
 package thor.model.io; 
 
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
 
 import thor.model.BufferedModel;
+import thor.model.geoset.BufferedMesh;
 
 // Polygon File Format
 class ModelReaderPly extends ModelReader {
@@ -11,6 +17,7 @@ class ModelReaderPly extends ModelReader {
 	public ModelReaderPly(String name, String extension) {
 		super(name, extension);
 	}
+	
 	public BufferedModel read(String filename) throws IOException {
 		/*
 		 * ================================================================ 
@@ -32,6 +39,82 @@ class ModelReaderPly extends ModelReader {
 		 * * return model;
 		 * ================================================================
 		 */
-		throw new IOException("PO3D-Pratica-1: file format not recognized");
+		
+		BufferedModel model = new BufferedModel(_name, _extension);
+		BufferedMesh mesh = new BufferedMesh();
+
+		Scanner root = new Scanner(new FileReader(filename));
+		if(!root.hasNext()) {
+			root.close();
+			throw new IOException("ModelReader: " + filename + " is empty");
+		}
+		
+		boolean endHeader = false;
+		int nVertices = 0;
+		int nFaces = 0;
+		
+		while(root.hasNext()) {
+			Scanner line = new Scanner(root.nextLine());
+			line.useLocale(Locale.US);
+			
+			String type = line.next();
+			
+			if(type.compareToIgnoreCase("element") == 0) {
+				type = line.next();
+				// Obtains the total number of Vertices
+				if(type.compareToIgnoreCase("vertex") == 0) {
+					nVertices = line.nextInt();
+				}
+				// Obtains the total number of Faces
+				else if(type.compareToIgnoreCase("face") == 0) {
+					nFaces = line.nextInt();
+				}
+			}
+			if(type.compareToIgnoreCase("property") == 0) {
+
+			}
+			if(type.compareToIgnoreCase("end_header") == 0) {
+				endHeader = true;
+			}
+			
+			if (endHeader) {
+				// Add vertices to mesh
+				for(int i = 0; i < nVertices; i++) {
+					Scanner vertexLine = new Scanner(root.nextLine());
+					vertexLine.useLocale(Locale.US);
+					
+					double x = vertexLine.nextDouble();
+					double y = vertexLine.nextDouble();
+					double z = vertexLine.nextDouble();
+					mesh.addVertex(x, y, z);
+					
+					vertexLine.close();
+				}
+				// Add faces to mesh
+				for(int j = 0; j < nFaces; j++) {
+					Scanner faceLine = new Scanner(root.nextLine());
+					faceLine.useLocale(Locale.US);
+					
+					List<Integer> v = new ArrayList<Integer>(); // vertices id's
+					List<Integer> vt = new ArrayList<Integer>();// texture coordinates id's
+					List<Float> vn = new ArrayList<Float>();// normal id's
+					
+					int nVertex = faceLine.nextInt();
+					
+					for(int k = 0; k < nVertex; k++) {
+						v.add(faceLine.nextInt());
+					}
+					
+					mesh.addFace(v, vt, vn);
+					faceLine.close();
+				}
+			}
+			line.close();
+		}
+		model.addMesh(mesh);
+		root.close();
+		
+		System.out.println("Model loaded");
+		return model;
 	}
 }
